f484fae1e7ba0fa261bba0bdee58c681fd17bc23
Jeff <jeffndl_23@outlook.com>
2021-05-29 01:31:55 +0100

    Added express error handling to bring errors to top

--- src/constants/errorConstants.js
+++ src/constants/errorConstants.js
@@ -1,8 +1,14 @@
 module.exports.errorsTemplates = [
   {
     
-    code: 500,
+    code: 404,
     internalCode: 'ERR001',
     message: "Unable to parse request parameters ",
+  },
+  {
+    
+    code: 500,
+    internalCode: 'ERR002',
+    message: "Unknown Error. Please Investigate. ",
   },
 ];
--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+const { createLogItem } = require("./utils/createLog");
 
 const donationRouter = require("./routes/donationRoutes");
 
 app.use("/api/donate", donationRouter);
 
-let server
-const port = 3003
+app.use((err, req, res, next) => {
+  if (err.expose === undefined) {
+    next(createLogItem("error", "ERR002"));
+  } else {
+    res.status(err.status).json({ error: err });
+  }
+});
 
-const setUp= async() => {
-    server = app.listen(port)
-    console.log(`I am listening on ${port}`)
+let server;
+const port = 3003;
 
-}
-setUp()
+const setUp = async () => {
+  server = app.listen(port);
+  console.log(`I am listening on ${port}`);
+};
+setUp();
 
-module.exports ={app, server}
+module.exports = { app, server };
